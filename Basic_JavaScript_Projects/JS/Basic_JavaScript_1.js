fbb1ecddbfae795384937efcc5be908bb28d55d0
Baoster3 <59859007+Baoster3@users.noreply.github.com>
2020-04-03 22:16:36 -0600

    Added Basic_HTML_1.html
    Added files with JavaScript for projects folder.

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    alert("I am an alert box; do it again!")
+}
+
+
+function myfunction() {
+    var x = 3
+    var y = (x + x)
+    alert("3 plus 3 equals " + y + " ! ");
+}
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.")
+
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\" "
++ "Dr. Seuss");
+
+
+var x =  " What\'s your favorite color? "
+var y = "and favorite number?"
+var z = (x + y)
+document.write(z)
