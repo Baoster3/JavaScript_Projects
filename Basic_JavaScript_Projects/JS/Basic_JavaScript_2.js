4b315be146c76c15a41f1d0ad2b7451fd9717200
Baoster3 <59859007+Baoster3@users.noreply.github.com>
2020-04-06 17:30:49 -0600

    Added JavaScript_2.js for Basic HTML 2
    This might be updated in a later challenge / assingment

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,10 @@
+function Myfunction() {
+    var str = "This text is blue!";
+    var result = str.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
+    {
+    var sentence = "I am learning";
+    sentence += " how to do many things with code!";
+    document.getElementById("Concatenate").innerHTML  = sentence;
+    }
+}
