79711b250eda300da3c8fa76cf66d1707109061b
Baoster3 <59859007+Baoster3@users.noreply.github.com>
2020-04-13 19:35:32 -0600

    Basic JavaScript for HTML 5
    This is for the HTML 5 file and to be reviewed by tech academy instructors.

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,32 @@
+document.write("Hello, World! ");
+document.write(typeof 10);
+
+document.write(2E400);
+
+document.write(-2E400);
+
+document.write(100<101);
+
+console.log(4 + 8);
+
+document.write("10" + 5);
+
+document.write(15 == 15);
+
+document.write(13 == 90);
+
+document.write(10 === 10);
+document.write("10" === 100);
+document.write("10" === 10);
+document.write("10" === "100");
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+document.write(5 > 2 && 20 > 10);
+
+document.write(5 > 3 && 10 >20);
+
+document.write(!5 < 10);
+document.write(!10 > 5);
